fc426fb105464699d8d4bf9abb5a87e18f8e302c
NihatHesenli <161558388+NihatHesenli@users.noreply.github.com>
2024-03-26 11:55:58 -0700

    Add files via upload

---
+++ Yeni klasör/ConsoleApp123/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace task_1
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+
+            }
+
+        }
+
+
+        class Vehicle
+        {
+            public string brand;
+            public string model;
+            public double mile;
+
+            public Vehicle(string brand, string model)
+            {
+                this.brand = brand;
+                this.model = model;
+                
+            }
+            public void ShowFullInfo()
+            {
+                Console.WriteLine($"brand:{brand}, model:{model}");
+            }
+        }
+
+
+
+         
+        class car : Vehicle
+        {
+            public int doorsNum;
+
+            public car(string brand, string model) :base(brand, model)
+            {
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+        class MotorCycle : Vehicle
+        {
+            public int WheelNum;
+
+            public MotorCycle(string brand, string model) : base(brand, model)
+            {
+
+
+            }
+        }
+
+
+    }
+
+}
